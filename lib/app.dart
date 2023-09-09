d2d64b010786b72f5527400585558c894481494d
spandan972 <141906406+spandan972@users.noreply.github.com>
2023-09-09 11:29:15 -0500

    Intialize

--- lib/app.dart
+++ lib/app.dart
@@ -7,7 +7,7 @@ import 'package:user_repository/user_repository.dart';
 class MainApp extends StatelessWidget {
   final UserRepository userRepository;
   const MainApp(this.userRepository, {super.key});
-
+//GIt try
   @override
   Widget build(BuildContext context) {
     return MultiRepositoryProvider(providers: [
