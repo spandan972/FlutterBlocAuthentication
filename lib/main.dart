a1d0e6a4f36d76395919bf6b75b72c7144478874
Spandan Datta <spandandatta18@gmail.com>
2023-09-11 22:52:44 -0500

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:bloctry/app.dart';
 import 'package:bloctry/simple_bloc_observer.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter/mhttps://github.com/spandan972/Bloc_Tryaterial.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
